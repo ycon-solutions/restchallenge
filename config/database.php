042039c04c518b7e144fc0864df9b743d4a990b7
travislykes <fjtravis@gmail.com>
2022-02-05 12:30:44 +0100

    Initialize project with ddev LDE

--- config/database.php
+++ config/database.php
@@ -91,6 +91,22 @@ return [
             'prefix_indexes' => true,
         ],
 
+        'ddev' => [
+            'driver' => 'mysql',
+            'host' => 'db',
+            'port' => '3306',
+            'database' => 'db',
+            'username' => 'db',
+            'password' => 'db',
+            'unix_socket' => '',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+        ],
+
     ],
 
     /*
